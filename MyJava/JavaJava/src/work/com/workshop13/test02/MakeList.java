5f2e5efd9816b50004b574a30bca71fe817c482d
Lee Chae Rin <chaerin0916@gmail.com>
2025-04-07 10:59:38 +0900

    Refactor : 스트림 활용해서 수정

--- MyJava/JavaJava/src/work/com/workshop13/test02/MakeList.java
+++ MyJava/JavaJava/src/work/com/workshop13/test02/MakeList.java
@@ -6,12 +6,12 @@ public class MakeList {
 	private ArrayList<Integer> al;
 	
 	public MakeList() {
-
+		// 멤버 변수 초기화
+		al = new ArrayList<>();
 	}
 
 	// Size 만큼의 정수형 데이터를 랜덤 하게 만들어 ArrayList에 넣는다.
 	public void makeArrayList(int size) {
-		al = new ArrayList<>(size);
 		// 무작위의 번호 발생 후 저장
 		// 10부터 19까지의 정수를 랜덤하게 만들어
 		for (int i = 0; i < size; i++) {
@@ -21,11 +21,15 @@ public class MakeList {
 
 	// ArrayList에 셋팅 된 정수들의 평균을 리턴 한다.
 	public double getAverage() {
-		double sum = 0;
-		for(int i:al) {
-			sum+=i;
-		}
-		return sum/al.size();
+//		double sum = 0;
+//		for(int i:al) {
+//			sum+=i;
+//		}
+//		return sum/al.size();
+		return al.stream()
+				.mapToInt(Integer::intValue)	// int로 변환
+				.average()
+				.orElse(0.0);	// 값이 없을 때 기본 값
 	}
 
 	public ArrayList<Integer> getList() {
